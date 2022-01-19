653418d4a92271704530ee205f7228817a1db3e3
PavloHorbatiuk <pavlo.horbatiuk@gmail.com>
2022-01-19 17:11:17 +0200

    material icon

--- src/Components/Common/FormsControl/FormsControls.tsx
+++ src/Components/Common/FormsControl/FormsControls.tsx
@@ -2,19 +2,30 @@ import React from "react";
 import { WrappedFieldProps } from "redux-form";
 import s from "./FormsControls.module.css";
 
-export const Textarea: React.FC<WrappedFieldProps> = ({
-  input,
-  meta,
-  ...props
-}) => {
+const FormControl:React.FC<WrappedFieldProps> = ({input, meta, children, ...props})=>{
   const hasError = meta.touched && meta.error;
 
   return (
     <div className={s.formControl + " " + (hasError ? s.error : " ")}>
       <div>
-        <textarea {...input} {...props} />
+        {children}
       </div>
       {hasError && <span>{meta.error}</span>}
     </div>
   );
+}
+
+export const Textarea: React.FC<WrappedFieldProps> = (props) => {
+  const {input, meta, children, ...restProps}=props
+  
+
+  return <FormControl {...props}><textarea {...input}{...restProps}/></FormControl>
+};
+
+
+export const Input: React.FC<WrappedFieldProps> = (props) => {
+  const {input, meta, children, ...restProps}=props
+  
+
+  return <FormControl {...props}><input {...input}{...restProps}/></FormControl>
 };
--- src/Components/Login/login.tsx
+++ src/Components/Login/login.tsx
@@ -1,48 +1,64 @@
 import React from "react";
-import {Field, InjectedFormProps, reduxForm} from "redux-form";
-import s from "./Login.module.css"
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
+import { Input } from "../Common/FormsControl/FormsControls";
+import { required } from "../utils/validators";
+import s from "./Login.module.css";
 
 type LoginFormType = {
-    Login: string;
-    password: string;
-    rememberMe: boolean;
-}
+  Login: string;
+  password: string;
+  rememberMe: boolean;
+};
 export let LoginForm: React.FC<InjectedFormProps<LoginFormType>> = (props) => {
-
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <div>
-                <Field className={s.input} placeholder={'Login'} name={"Login"} component={'input'}/>
-            </div>
-            <div>
-                <Field className={s.input} placeholder={"Password"} name={"password"} component={'input'}/>
-            </div>
-            <div>
-                <Field className={s.input} component={'input'} name={'rememberMe'} type={'checkbox'}/> remember me
-            </div>
-            <div>
-                <button>Login</button>
-            </div>
-        </form>
-    )
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field
+          validate={[required]}
+          className={s.input}
+          placeholder={"Login"}
+          name={"Login"}
+          component={Input}
+        />
+      </div>
+      <div>
+        <Field
+          validate={[required]}
+          className={s.input}
+          placeholder={"Password"}
+          name={"password"}
+          component={Input}
+        />
+      </div>
+      <div>
+        <Field
+          className={s.input}
+          component={Input}
+          name={"rememberMe"}
+          type={"checkbox"}
+        />{" "}
+        remember me
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
 };
 
 const LoginReduxForm = reduxForm<LoginFormType>({
-    form: 'Login'
-})(LoginForm)
-
+  form: "Login",
+})(LoginForm);
 
 const Login = () => {
-    const onSubmit = (formData: LoginFormType) => {
-        console.log(formData)
-    }
-    return (
-        <div>
-            <h1> Login</h1>
-            <LoginReduxForm
-                onSubmit={onSubmit}
-            />
-        </div>
-    )
-}
+  const onSubmit = (formData: LoginFormType) => {
+    console.log(formData);
+  };
+  return (
+    <div>
+      <h1> Login</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+  );
+};
 export default Login;
