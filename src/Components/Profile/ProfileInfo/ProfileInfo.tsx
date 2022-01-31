b0823ed19152e8e7c85b71a8f50f0ce277a46ff5
PavloHorbatiuk <pavlo.horbatiuk@gmail.com>
2022-01-31 02:40:57 +0200

    useEffect

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,46 +1,47 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 import Preloader from "../../Common/Preloader/Preloader.sx";
-import { getAxiosProfileType } from "../../../Redux/Profile-reducer";
+import {getAxiosProfileType} from "../../../Redux/Profile-reducer";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatuswirhHooks";
 
 type ProfileInfoType = {
-  profile: null | getAxiosProfileType;
-  status: string;
-  updateStatus: (status: string) => void;
+    profile: null | getAxiosProfileType;
+    status: string;
+    updateStatus: (status: string) => void;
 };
 
 export function ProfileInfo(props: ProfileInfoType) {
-  if (!props.profile) {
-    return <Preloader />;
-  }
+    if (!props.profile) {
+        return <Preloader/>;
+    }
 
-  return (
-    <div className={s.ProfileStyle}>
-      <div className={s.ProfileInfo}></div>
-      <div>
-        <img
-          className={s.img}
-          src={props.profile.photos.small}
-          alt="ProfileFoto"
-        />
-        <ul>
-          <h2>Contacts</h2>
-          <li>
-            <span>{props.profile.contacts.github}</span>
-          </li>
-          <li>
-            <span>{props.profile.contacts.facebook}</span>
-          </li>
-          <li>
-            <span>{props.profile.contacts.instagram}</span>
-          </li>
-          <ProfileStatus
-            updateStatus={props.updateStatus}
-            status={props.status}
-          />
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className={s.ProfileStyle}>
+            <div className={s.ProfileInfo}></div>
+            <div>
+                <img
+                    className={s.img}
+                    src={props.profile.photos.small}
+                    alt="ProfileFoto"
+                />
+                <ul>
+                    <h2>Contacts</h2>
+                    <li>
+                        <span>{props.profile.contacts.github}</span>
+                    </li>
+                    <li>
+                        <span>{props.profile.contacts.facebook}</span>
+                    </li>
+                    <li>
+                        <span>{props.profile.contacts.instagram}</span>
+                    </li>
+                    <ProfileStatusWithHooks
+                        updateStatus={props.updateStatus}
+                        status={props.status}
+                    />
+                </ul>
+            </div>
+        </div>
+    );
 }
